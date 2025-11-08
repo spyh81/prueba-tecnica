76e80c2d339da15ac6e97544e4af1dd3ba5fda1d
spyh81 <gjimenezperez0@gmail.com>
2025-11-08 16:04:13 +0100

    Subiendo el resto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "empleados",
+        pathMatch: "full"
+    },
+    {
+        path: "empleados",
+        loadComponent: () => import('./modules/empleados/pages/empleados/empleados.component').then(
+            (m) => m.EmpleadosComponent,
+        )
+    },
+    {
+        path: "proyectos",
+        loadComponent: () => import('./modules/proyectos/pages/proyectos/proyectos.component').then(
+            (m) => m.ProyectosComponent,
+        )
+    },
+    {
+        path: "tareas",
+        loadComponent: () => import('./modules/tareas/pages/tareas/tareas.component').then(
+            (m) => m.TareasComponent,
+        )
+    },
+];
---
+++ src/app/modules/tareas/pages/tareas/tareas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tareas',
+  standalone: true,
+  imports: [],
+  templateUrl: './tareas.component.html',
+  styleUrl: './tareas.component.css'
+})
+export class TareasComponent {
+
+}
