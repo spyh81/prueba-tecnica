19f35841842d33afd0f6472a32e5026990824f3f
spyh81 <gjimenezperez0@gmail.com>
2025-11-11 13:04:48 +0100

    adaptado componente listado de angular material y creación de servicios

--- src/app/modules/tareas/pages/tareas/tareas.component.ts
+++ src/app/modules/tareas/pages/tareas/tareas.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ListadoComponent } from '../../../../shared/listado/listado.component';
 
 @Component({
   selector: 'app-tareas',
   standalone: true,
-  imports: [],
+  imports: [ListadoComponent],
   templateUrl: './tareas.component.html',
   styleUrl: './tareas.component.css'
 })
